06e9b0f9eceacc8041b3bf1837dc0a97bcb223c7
Felquis Gimenes <frgformenton@gmail.com>
2016-04-14 13:50:04 -0300

    mediator

--- js/index.js
+++ js/index.js
@@ -1,3 +1,20 @@
 'use strict'
 
-console.log('Template')
+var count = {
+	total: 0
+}
+
+mediator.installTo(count)
+
+var buttonElement = document.querySelector('button')
+var totalElement = document.querySelector('.total')
+var onButtonClick = function (event) {
+	count.total += 1
+	count.publish('increase', count.total)
+}
+
+count.subscribe('increase', function(number) {
+    totalElement.innerHTML = number
+});
+
+buttonElement.addEventListener('click', onButtonClick)
